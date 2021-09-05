572d612f40117e9aa04dd913c020689af61190e6
kautsar_masuara <kautsarmasuara@gmail.com>
2021-09-05 19:23:41 +0800

    configuration development database postgres

---
+++ migrations/20210905112013-create-hospital.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Hospitals', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      pointX: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      pointY: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      telephonNumber: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Hospitals');
+  }
+};
